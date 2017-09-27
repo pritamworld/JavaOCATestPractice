c315671edb374cf8e109d9eb64fec2be0d89bd96
pritesh.patel <pritam.world@gmail.com>
2017-09-27 13:44:35 -0400

    Believe or not. It's true.

--- src/javaocatestpractice/Finalizer.java
+++ src/javaocatestpractice/Finalizer.java
@@ -5,6 +5,15 @@
  */
 package javaocatestpractice;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author pritesh.patel
@@ -22,8 +31,11 @@ public class Finalizer {
         Finalizer f = new Finalizer();
         f.s = new String("Pritesh Patel");
         //f = null;
-        String n;
+        String n = null;
         System.out.println(f);
+        Object[] aa = {new Finalizer(), new Finalizer()};
+        System.out.println(new Object());
+        System.out.println(aa);
         System.gc();
 
         int a = 1_234;
@@ -137,5 +149,75 @@ public class Finalizer {
         byte a0 = 40, b = 50;
         byte sum = (byte) ((byte) a0 + b);
         System.out.println(sum);
+        System.out.println("-------------");
+        String tag = Finalizer.class.getName() + '@' + Integer.toHexString(Finalizer.class.hashCode());
+        System.out.println(tag);
+
+        System.out.println("-------------");
+        ArrayList list = new ArrayList();
+        list.add("hawk"); // [hawk]
+        list.add("hawk"); // [hawk]
+        list.add(Boolean.TRUE); // [hawk, true]
+        list.add(100);
+        list.add(false);
+        list.add(new Date());
+        System.out.println(list);
+        list.remove("hawk");
+        System.out.println(list);
+
+        System.out.println("-------------");
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+        LocalDate date1 = LocalDate.of(2015, Month.JANUARY, 20);
+        LocalDate date2 = LocalDate.of(2015, 1, 20);
+        LocalDate start = LocalDate.of(2015, Month.JANUARY, 1);
+        LocalDate end = LocalDate.of(2015, Month.MARCH, 30);
+        performAnimalEnrichment(start, end);
+        System.out.println("-------------");
+        Period period = Period.ofMonths(1); // create a period
+        performAnimalEnrichment(start, end, period);
+        System.out.println("-------------");
+        LocalDate date = LocalDate.of(2020, Month.JANUARY, 20);
+        System.out.println(date.getDayOfWeek()); // MONDAY
+        System.out.println(date.getMonth()); // JANUARY
+        System.out.println(date.getYear()); // 2020
+        System.out.println(date.getDayOfYear()); // 20
+        System.out.println("-------------");
+        LocalTime time = LocalTime.of(11, 12, 34);
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+        System.out.println(date
+                .format(DateTimeFormatter.ISO_LOCAL_DATE));
+        System.out.println(time.format(DateTimeFormatter.ISO_LOCAL_TIME));
+        System.out.println(dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        System.out.println("-------------");
+        LocalDate d1 = LocalDate.of(2020, Month.JANUARY, 20);
+        LocalTime t1 = LocalTime.of(11, 12, 34);
+        LocalDateTime dateTime1 = LocalDateTime.of(d1, t1);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMMM dd, yyyy, hh:mm");
+        System.out.println(dateTime1.format(fmt)); // January 20, 2020, 11:12
+        
+        System.out.println("------ Excellent Knowledge about Static Members -------");
+        SampleStatic s = new SampleStatic();
+        System.out.println(s.getName());
+        s = null;
+        s.NAME = "Pritesh";
+        System.out.println(SampleStatic.getName());
+    }
+
+    private static void performAnimalEnrichment(LocalDate start, LocalDate end) {
+        LocalDate upTo = start;
+        while (upTo.isBefore(end)) { // check if still before end
+            System.out.println("give new toy: " + upTo);
+            upTo = upTo.plusMonths(1); // add a month
+        }
+    }
+
+    private static void performAnimalEnrichment(LocalDate start, LocalDate end, Period period) { // uses the generic period
+        LocalDate upTo = start;
+        while (upTo.isBefore(end)) {
+            System.out.println("give new toy: " + upTo);
+            upTo = upTo.plus(period); // adds the period
+        }
     }
 }
---
+++ src/javaocatestpractice/SampleStatic.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaocatestpractice;
+
+/**
+ *
+ * @author pritesh.patel
+ */
+public class SampleStatic {
+    
+    public static String NAME = "Pritesh Patel";
+    public static String getName(){
+        return NAME;
+    }
+}
